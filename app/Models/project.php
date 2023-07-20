b02fe8b19024ac8c88ea27a5604535875a0c5a4b
Alecpu92 <alessandro_belfiore@hotmail.it>
2023-07-20 18:39:26 +0200

    importato il model su dbseeders

--- app/Models/project.php
+++ app/Models/project.php
@@ -17,5 +17,5 @@ class project extends Model
         "creator"
 
 
-    ]
+    ];
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\project;
+
 class DatabaseSeeder extends Seeder
 {
     /**
